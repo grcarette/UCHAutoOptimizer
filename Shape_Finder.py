21c34b6c4cb4efe725952069f17b382321a749c9
Greg Carette <grcarette1@gmail.com>
2022-06-29 23:45:21 -0400

    Added vertice finder, cropped shapes to improve speed

--- Shape_Finder.py
+++ Shape_Finder.py
@@ -1,49 +1,65 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from anytree import Node, RenderTree, NodeMixin, LevelOrderIter
+import itertools, operator
 
-def tuple_operation(a, b, op):
-    if op == '-':
-        return tuple(map(operator.sub, a, b))
-    elif op == '+': 
-        return tuple(map(operator.add, a, b))
-    elif op == 'mean':
-        return tuple(map(operator.truediv,(map(operator.add, a, b)),(2,2)))
-    elif op == '*':
-        return tuple(map(operator.mul, a, b))
-
-def plot_matrix(matrix):
+def plot_matrix(matrix, vertices = None):
+    if vertices != None:
+        plt.scatter(*zip(*vertices))
     plt.imshow(matrix)
     plt.show()
-
-
-def produce_shape(opt_mat, shape_mat, position): 
-    if opt_mat[position] != 0:
-        iterable_size = (tuple([index -1 for index in np.shape(opt_mat)])) #np.shape produces shape of matrix starting from 1, change to start from 0
-        adjacent_tiles = [(position[0]+1, position[1]),(position[0]-1, position[1]),(position[0], position[1]+1),(position[0], position[1]-1)]
-        shape_mat[position] = 1
-        opt_mat[position] = 0
-        for checkable_tile in [tile for tile in adjacent_tiles if all(0 <= tile[x] <= iterable_size[x] for x in range(len(iterable_size)))]:
-            produce_shape(opt_mat, shape_mat, checkable_tile)
-    return shape_mat
 
 def find_shapes(opt_mat):
     shape_list = []
     opt_mat_rows = np.size(opt_mat,0)
     opt_mat_cols = np.size(opt_mat,1)
-    shape_mat = np.zeros((opt_mat_rows, opt_mat_cols))
-    while not np.array_equiv(opt_mat, np.zeros((opt_mat_rows,opt_mat_cols))):
-        for ind_col in range(opt_mat_cols):
-            for ind_row in range(opt_mat_rows):
-                if opt_mat[ind_row,ind_col] != 0:
-                    shape_list.append(produce_shape(opt_mat, shape_mat, (ind_row,ind_col)))
-                    shape_mat = np.zeros((opt_mat_rows, opt_mat_cols))
+    for ind_col in range(opt_mat_cols):
+        for ind_row in range(opt_mat_rows):
+            if opt_mat[ind_row,ind_col] == 1:
+                shape_list.append((produce_shape(opt_mat, np.zeros((opt_mat_rows, opt_mat_cols)),(ind_row,ind_col))))
     return shape_list
 
+def produce_shape(opt_mat, shape_mat, position): 
+    adjacent_tiles = [(position[0]+1, position[1]),(position[0]-1, position[1]),(position[0], position[1]+1),(position[0], position[1]-1)]
+    shape_mat[position] = 1
+    opt_mat[position] = 2
+    for checkable_tile in [tile for tile in adjacent_tiles if opt_mat[tile] == 1]:
+        produce_shape(opt_mat, shape_mat, checkable_tile)
+    return shape_mat
+
+def crop_shape(shape):
+    row_lst = [np.any(row) for row in shape]
+    col_lst = [np.any(col) for col in shape.T]
+
+    ind_rowa = row_lst.index(True)-1
+    ind_rowb = len(row_lst) - row_lst[::-1].index(True)+1
+    ind_cola = col_lst.index(True)-1
+    ind_colb = len(col_lst) - col_lst[::-1].index(True)+1
+
+    shape = shape[ind_rowa:ind_rowb,ind_cola:ind_colb]
+
+    return shape, [ind_cola,ind_colb,ind_rowa,ind_rowb]
+
+def shape_to_vertices(shape, origin):
+    shape_b = shape[0:-1,0:-1]
+    vertices = []
+    for ind_col in range(np.size(shape_b,1)):
+        for ind_row in range(np.size(shape_b,0)):
+            sub_mat = shape[ind_row:ind_row+2,ind_col:ind_col+2]
+            sub_mat_count = np.count_nonzero(sub_mat)
+            if sub_mat_count == 0 or sub_mat_count == 4:
+                pass
+            elif sub_mat_count == 1 or sub_mat_count == 3:
+                vertices.append((ind_col+0.5,ind_row+0.5))
+            elif np.count_nonzero(sub_mat-sub_mat.T) == 0:
+                vertices.append((ind_col+0.5,ind_row+0.5))
+
+
 if __name__ == "__main__":
-    test_matrix = np.random.randint(2, size=(16,16))
+    test_matrix = np.random.randint(2, size=(128,128))
+    test_matrix = np.pad(test_matrix, pad_width = 1, mode='constant', constant_values=0)
+    plot_matrix(test_matrix)
     shape_list = find_shapes(test_matrix)
     for shape in shape_list:
-        plot_matrix(shape)
-
-    
+        cropped_shape, position = crop_shape(shape)
+        shape_vertices = shape_to_vertices(cropped_shape, position)
+    print('done')
